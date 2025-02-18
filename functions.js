f3452453aadb92bc0553d1d957e99472dbe02dc2
Yeahnick <79931245+Yeahnick@users.noreply.github.com>
2025-02-18 12:59:17 -0500

    Alot of tweaks

--- functions.js
+++ functions.js
@@ -10,25 +10,31 @@ function sendemail(){
 }
 function verifyFormat(){
 
+    var result
     /* Empty fields */
     if(document.getElementById("fname").value == ""){
         document.getElementById("fname").style.border = "1px solid tomato"
-        return false;
+        result = false
     }
     if(document.getElementById("email").value == ""){
         document.getElementById("email").style.border = "1px solid tomato"
-        return false;
+        result =  false
     }
     if(document.getElementById("text").value == ""){
         document.getElementById("text").style.border = "1px solid tomato"
-        return false;
+       result = false
     }
     /* Email format*/
     const re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
     if(re.test(document.getElementById("email").value)){
-        return true
+        result = true
     }else{
         document.getElementById("email").style.border = "1px solid tomato"
-        return false
+        result = false
     }
+    return result
+}
+
+function ScrollTo(id){
+    document.getElementById(`${id}`).scrollIntoView({behavior: "smooth"});
 }
